49ab7fb8bcb5f918c7d56a90281221467a4e03ee
zulkarnaini404 <125197866+zulkarnaini404@users.noreply.github.com>
2023-02-12 21:01:46 +0700

    Add files via upload

---
+++ romz/my_info.py
@@ -0,0 +1,39 @@
+from xyz import *
+from romz.banner import Banner as xyz
+#--- INFO
+def ingfoh():
+	xyz().logo3()
+	tulis(Panel(
+f"""{Te} {U}•{H} Info script {M}:{H}
+
+ {P}-{O} author      {M}:{H} Romi Afrizal
+ {P}-{O} instagram   {M}:{H} romz_xyz
+ {P}-{O} facebook    {M}:{H} facebook.com/romi.afrizal.102
+ {P}-{O} fanspage    {M}:{H} facebook.com/100022086172556
+ {P}-{O} whatsap     {M}:{H} +6281273018924
+ {P}-{O} github      {M}:{H} github.com/Mark-Zuck
+ {P}-{O} script name {M}:{H} bff-2
+ {P}-{O} version     {M}:{H} 3.11
+ {P}-{O} update pada {M}:{H} 25 januari 2023
+
+{P}+{M} ---------------------------------------- {P}+{H}
+             TIDAK SUPORT KARTU 
+{P}+{M} ---------------------------------------- {P}+{H}
+{P}-{O} Kartu Telkomsel tidak suport untuk crack
+  jadi wajar jika tidak dapat hasil atau lama
+  pada saat crack, Karena rawan spam.
+  Rekomendasi kartu Indosat, M3, Axis, XL
+ 
+{P}+{M} ---------------------------------------- {P}+{H}
+                MODE PESAWAT
+{P}+{M} ---------------------------------------- {P}+{H}
+{P}-{O} Jika gunakan mode pesawat itu guna nya 
+  akan melewati beberapa ID dan merubah IP 
+  kita pada saat proses crack. Cukup gunakan
+  mode pesawat 1-2 detik saja. Jika gunakan 
+  mode pesawat terlalu lama maka akan semakin
+  banyak ID yg terlewatkan. Maka dari itu cukup
+  gunakan 1-2 detik saja.
+  
+{M}!{H} Jika bug/error pada script harap lapor saya""",
+	title=f'{Te}{M}[ {H}INFO {M}]',style='#AF00FF'))
